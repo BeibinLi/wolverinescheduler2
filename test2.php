5aff8b6204e5f8c682d051635b4ccbcff5e5c09d
Carl Chan <carlchan@umich.edu>
2015-01-17 16:59:07 -0500

    Update test2.php

--- test2.php
+++ test2.php
@@ -6,6 +6,22 @@
 
 <?php
 echo "Hello World!";
+
+$server = "tcp:<value of SERVER from section above>";
+$user = "<value of USERNAME from section above>"@SERVER_ID;
+$pwd = "password";
+$db = "testdb";
+
+$conn = sqlsrv_connect($server, array("UID"=>$user, "PWD"=>$pwd, "Database"=>$db));
+
+if($conn === false){
+    #die(print_r(sqlsrv_errors()));
+    echo "Failure!";
+} else {
+    echo "Success!";
+}
+
+
 ?>
 
 </body>
