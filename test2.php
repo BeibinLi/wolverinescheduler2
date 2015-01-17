236dc4c99c5bcce1f2d5a05c68d23f4e68f29096
Carl Chan <carlchan@umich.edu>
2015-01-17 18:00:12 -0500

    Update test2.php

--- test2.php
+++ test2.php
@@ -22,8 +22,8 @@
 	$conn = sqlsrv_connect($server, array("UID"=>$user, "PWD"=>$pwd, "Database"=>$db));
 
 	if($conn === false){
-	    die(print_r(sqlsrv_errors()));
-	    echo "Failure!";
+		echo "Failure!";
+		die(print_r(sqlsrv_errors()));
 	} else {
 		echo "Success!";
 	}
