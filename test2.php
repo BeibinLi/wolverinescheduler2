9162c1f8e796f4ac67810e8ac3986ce6a90de1bb
Carl Chan <carlchan@umich.edu>
2015-01-17 18:11:51 -0500

    Update test2.php

--- test2.php
+++ test2.php
@@ -14,16 +14,11 @@
 
 <?php
 
-
 $conn = new PDO ( \"sqlsrv:server = tcp:rd4vxrj1mk.database.windows.net,1433; Database = School\", \"SQLAdmin\", \"Mhacks12345\");\r\n    $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );\r\n}\r\ncatch ( PDOException $e ) {\r\n   print( \"Error connecting to SQL Server.\" );\r\n   die(print_r($e));\r\n}\r\n\rSQL Server Extension Sample Code:\r\n\r\n$connectionInfo = array(\"UID\" => \"SQLAdmin@rd4vxrj1mk\", \"pwd\" => \"Mhacks12345\", \"Database\" => \"School\", \"LoginTimeout\" => 30, \"Encrypt\" => 1);\r\n$serverName = \"tcp:rd4vxrj1mk.database.windows.net,1433\";\r\n$conn = sqlsrv_connect($serverName, $connectionInfo);
 
-	if($conn === false){
-		echo "Failure!";
-	} else {
-		echo "Success!";
-	}
+if($conn === false){
+	echo "Failure!";
+} else {
+	echo "Success!";
+}
 
-?>
-
-</body>
-</html>
