2adaf1980287874cbfcb2b6f13e3bf2d5cbb5956
Carl Chan <carlchan@umich.edu>
2015-01-17 17:05:50 -0500

    Update test2.php

--- test2.php
+++ test2.php
@@ -16,14 +16,14 @@ $user = "<value of USERNAME from section above>"@SERVER_ID;
 $pwd = "password";
 $db = "testdb";
 
-$conn = sqlsrv_connect($server, array("UID"=>$user, "PWD"=>$pwd, "Database"=>$db));
+// $conn = sqlsrv_connect($server, array("UID"=>$user, "PWD"=>$pwd, "Database"=>$db));
 
-if($conn === false){
-    #die(print_r(sqlsrv_errors()));
-    echo "Failure!";
-} else {
-    echo "Success!";
-}
+// if($conn === false){
+//     #die(print_r(sqlsrv_errors()));
+//     echo "Failure!";
+// } else {
+//     echo "Success!";
+// }
 
 
 ?>
