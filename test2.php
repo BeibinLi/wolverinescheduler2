d67f52fd1739d0a9fb56a23700fbde996f61c9ba
Carl Chan <carlchan@umich.edu>
2015-01-17 18:06:58 -0500

    Update test2.php

--- test2.php
+++ test2.php
@@ -14,12 +14,8 @@
 
 <?php
 
-	$server = "tcp:<value of SERVER from section above>";
-	$user = "<value of USERNAME from section above>";
-	$pwd = "password";
-	$db = "testdb";
 
-	$conn = sqlsrv_connect($server, array("UID"=>$user, "PWD"=>$pwd, "Database"=>$db));
+$conn = new PDO ( \"sqlsrv:server = tcp:rd4vxrj1mk.database.windows.net,1433; Database = School\", \"SQLAdmin\", \"{your_password_here}\");\r\n    $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );\r\n}\r\ncatch ( PDOException $e ) {\r\n   print( \"Error connecting to SQL Server.\" );\r\n   die(print_r($e));\r\n}\r\n\rSQL Server Extension Sample Code:\r\n\r\n$connectionInfo = array(\"UID\" => \"SQLAdmin@rd4vxrj1mk\", \"pwd\" => \"{your_password_here}\", \"Database\" => \"School\", \"LoginTimeout\" => 30, \"Encrypt\" => 1);\r\n$serverName = \"tcp:rd4vxrj1mk.database.windows.net,1433\";\r\n$conn = sqlsrv_connect($serverName, $connectionInfo);
 
 	if($conn === false){
 		echo "Failure!";
