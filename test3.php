d6d658be287e0c91d8386225dce547bd2011207d
qingzhouhack <245601716@qq.com>
2015-01-17 22:01:37 -0500

    Update test3.php

--- test3.php
+++ test3.php
@@ -30,10 +30,9 @@
       $dpt = $_POST['dpt'];
       $coursenum = $_POST['coursenum'];
       $coursename = $dpt . " " . $coursenum;
-      echo $coursename;
       $sql_select = "SELECT coursename, credits FROM courses
       WHERE coursename = '?'";
-      $stmt = $conn->prepare($sql_select);
+      $stmt = $conn->query($sql_select);
       $stmt->bindValue(1, $coursename);
       $courses = $stmt->fetchAll();
       if(count($courses) > 0) {
