50ea4b73ebdcafc27066a0ab75d4b0275cf7fde9
beibin79 <beibin79@gmail.com>
2015-01-17 20:48:41 -0500

    Update test.php

--- test.php
+++ test.php
@@ -27,21 +27,21 @@ class Course{
     public $coursename;
     // public $lectures;
     
-    // function __construct($credit_in, $coursename_in){
-    //     $this->credit = $credit_in;
-    //     $this->coursename = $coursename_in
+    function __construct2($credit_in, $coursename_in){
+        $this->credit = $credit_in;
+        $this->coursename = $coursename_in
         
-    //     echo "Course created: ";
-    //     echo $this->coursename;
-    //     echo "\r\n";
-    // }
+        echo "Course created: ";
+        echo $this->coursename;
+        echo "\r\n";
+    }
     
     function add($lec){
         array_push($this->lectures, $lec);
     }
 }
 
-// $eecs = new Course(4, "EECS 280");
+$eecs = new Course(4, "EECS 280");
 
 
 /*
