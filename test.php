7ef9253685b8d02275695985a2aecf2796fbc0f0
beibin79 <beibin79@gmail.com>
2015-01-18 00:16:04 -0500

    Update test.php

--- test.php
+++ test.php
@@ -223,16 +223,16 @@ $temp_sol = array();
 find_course_combination($allCourse, -1, 12, $temp_sol);
 // echo "THE SOLUTION IS: "; var_dump($solutions);
 
-// for($i=0; $i < count($solutions); $i++){
-//     $total_credits = 0;
-//     for($j=0; j<count($solutions[$i]); $j++){
-//         $sch = $solutions[$i][$j];
-//         echo $sch->coursename; echo " ";
-//     }
-//     echo "total: "; echo $total_credits; echo "<br>";
+for($i=0; $i < count($solutions); $i++){
+    $total_credits = 0;
+    for($j=0; $j<count($solutions[$i]); $j++){
+        $sch = $solutions[$i][$j];
+        echo $sch->coursename; echo " ";
+    }
+    echo "total: "; echo $total_credits; echo "<br>";
     
-//     $dumb = array();
-//     $schedule = array(); //not necessary!
+    $dumb = array();
+    $schedule = array(); //not necessary!
 //     // find_time($solutions[$i], -1, $dumb);
     
 //     // debug_schedule();
@@ -240,7 +240,7 @@ find_course_combination($allCourse, -1, 12, $temp_sol);
 
 // debug_solutions();
 
-echo "update3"; echo "<br>"; //new line
+echo "update"; echo "<br>"; //new line
 
 
 ?>
