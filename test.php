648a0540d4f785f6269bb985464c2e63b83ac39b
beibin79 <beibin79@gmail.com>
2015-01-17 21:09:26 -0500

    Update test.php

--- test.php
+++ test.php
@@ -13,14 +13,14 @@ class Lecture{
     public $start_time;
     public $end_time;
     
-    // function __construct() 
-    // { 
-    //     $a = func_get_args(); 
-    //     $i = func_num_args(); 
-    //     if (method_exists($this,$f='__construct'.$i)) { 
-    //         call_user_func_array(array($this,$f),$a); 
-    //     } 
-    // } 
+    function __construct() 
+    { 
+        $a = func_get_args(); 
+        $i = func_num_args(); 
+        if (method_exists($this,$f='__construct'.$i)) { 
+            call_user_func_array(array($this,$f),$a); 
+        } 
+    } 
     
     // function __construct2($a1,$a2){
     //     echo 'new Lecture created ';
