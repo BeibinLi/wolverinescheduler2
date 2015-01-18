887fd2c1e8e264a6b057046b52d0dcbcd1ffaa1c
beibin79 <beibin79@gmail.com>
2015-01-18 00:34:42 -0500

    SQL

--- test.php
+++ test.php
@@ -183,36 +183,92 @@ function debug_schedule(){
 }
 
 
-//begin main
-$c1 = new Course("EECS 280", 4);
-$c2 = new Course("EECS 370", 4);
-$c3 = new Course("MATH 412", 3);
-$c4 = new Course("MATH 500", 3);
+// //begin main
+// $c1 = new Course("EECS 280", 4);
+// $c2 = new Course("EECS 370", 4);
+// $c3 = new Course("MATH 412", 3);
+// $c4 = new Course("MATH 500", 3);
 
-$L1 = new Lecture("EECS 280", 8, 9);
-$L2 = new Lecture("EECS 280", 9, 10);
+// $L1 = new Lecture("EECS 280", 8, 9);
+// $L2 = new Lecture("EECS 280", 9, 10);
 
-$c1->add($L1);
-$c1->add($L2);
-// var_dump($c1);
-// var_dump($c1->lectures);
+// $c1->add($L1);
+// $c1->add($L2);
+// // var_dump($c1);
+// // var_dump($c1->lectures);
 
-$L3 = new Lecture("EECS 370", 10, 11);
-$L4 = new Lecture("EECS 370", 12, 15);
-$c2->add($L3);
-$c2->add($L4);
+// $L3 = new Lecture("EECS 370", 10, 11);
+// $L4 = new Lecture("EECS 370", 12, 15);
+// $c2->add($L3);
+// $c2->add($L4);
 
-$L5 = new Lecture("MATH 412", 13, 15);
-$L6 = new Lecture("MATH 412", 8, 11);
-$c3->add($L5);
-$c3->add($L6);
+// $L5 = new Lecture("MATH 412", 13, 15);
+// $L6 = new Lecture("MATH 412", 8, 11);
+// $c3->add($L5);
+// $c3->add($L6);
 
-$L6 = new Lecture("MATH 500", 15, 16);
-$L7 = new Lecture("MATH 500", 10, 12);
-$L8 = new Lecture("MATH 500", 11, 14);
-$c4->add($L6);
-$c4->add($L7);
-$c4->add($L8);
+// $L6 = new Lecture("MATH 500", 15, 16);
+// $L7 = new Lecture("MATH 500", 10, 12);
+// $L8 = new Lecture("MATH 500", 11, 14);
+// $c4->add($L6);
+// $c4->add($L7);
+// $c4->add($L8);
+
+
+//START READING USER INPUT
+
+$inputs = array();
+
+
+// START SQL FETCH
+error_reporting(-1);
+ini_set('display_errors', true);
+echo "Hello World!";
+
+$server = "tcp:rd4vxrj1mk.database.windows.net";
+$user = "SQLAdmin";
+$pwd = "Mhacks12345";
+$db = "MHacks2015";
+
+$conn = sqlsrv_connect($server, array("UID"=>$user, "PWD"=>$pwd, "Database"=>$db));
+
+if($conn === false){
+	echo "Failure!";
+} else {
+	echo "Success!";
+}
+
+
+
+foreach($inputs as $class){
+    $sql = "SELECT coursename, credits
+    FROM courses
+    WHERE coursename = 4;"; //???
+
+    $stmt = sqlsrv_query( $conn, $sql);
+    if( $stmt === false ) {
+        echo "Query failed!";
+    } else {
+        echo "Query successful";
+    }
+
+echo "\r\n";
+
+// Make the first (and in this case, only) row of the result set available for reading.
+while( sqlsrv_fetch( $stmt ) ) {
+
+	// Get the row fields. Field indeces start at 0 and must be retrieved in order.
+	// Retrieving row fields by name is not supported by sqlsrv_get_field.
+	$name = sqlsrv_get_field( $stmt, 0);
+	echo "$name: ";
+	echo "\r\n";
+
+	$comment = sqlsrv_get_field( $stmt, 1);
+	echo $comment;
+	echo "\r\n";
+}
+
+}//end foreach inputs
 
 
 $allCourse = array($c1, $c2, $c3, $c4);
@@ -231,15 +287,15 @@ for($i=0; $i < count($solutions); $i++){
     for($j=0; $j<count($solutions[$i]); $j++){
         $sch = $solutions[$i][$j];
         $total_credits += $sch->credit;
-        echo $sch->coursename; echo " ";
+        // echo $sch->coursename; echo " ";
     }
-    echo "total: "; echo $total_credits; echo "<br>";
+    // echo "total: "; echo $total_credits; echo "<br>";
     $dumb = array();
 
     find_time($solutions[$i], -1, $dumb);
 }
 
-var_dump($schedule);
+// var_dump($schedule);
 
 
 
