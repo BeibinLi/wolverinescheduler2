cb2818b0b8b495a1a1de6f92a5427830a1e9de25
beibin79 <beibin79@gmail.com>
2015-01-17 21:49:01 -0500

    Update test.php

--- test.php
+++ test.php
@@ -129,7 +129,7 @@ function debug_solutions()
     for($i=0; $i < count($solutions); $i++){
         $total_credit = 0;
         for($j=0; $j < count($solutions[i]); $j++){
-            echo << $solutions[$i][$j]->coursename; echo " ";
+            echo << ($solutions[$i][$j])->coursename; echo " ";
         //     $total_credit = $total_credit + $solutions[i][j]->credit;
         }
         echo "total: " << $total_credit ; echo "<br>"; //new line
