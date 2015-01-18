4069b9a2ae50578253372290c47623a5c9e97b03
qingzhouhack <245601716@qq.com>
2015-01-18 04:53:08 -0500

    Update index.php

--- schedulizer/index.php
+++ schedulizer/index.php
@@ -74,7 +74,8 @@ Released : 20120902
 </tr>
 </table>
 <input id="schedule_button" type="submit" value="CLICK TO SCHEDULE" style="background-color: #FFD700; width:40%; height: 50px;margin:0px; border:0px; font-size:1.6em; font-family: 'Abel', Arial; font-weight:bold" ></input>
-<input type="reset" value="Clear" style="border-radius:5px; background-color: #FFD700; width:10%; height: 50px;margin:0px; border:0px; font-size:1.6em; font-family: 'Abel', Arial; font-weight:bold">
+&nbsp;&nbsp;
+<input type="reset" value="Clear" style="border-radius:5px; background-color: #B80000; width:10%; height: 50px;margin:0px; border:0px; font-size:1.6em; font-family: 'Abel', Arial; font-weight:bold">
 </form>
 </center>
 
